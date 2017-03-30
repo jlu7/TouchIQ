1f6e0fea7a3e67950ed23edd71ddf19718b36d91
Tonton Rue <tontonrue@playstudios.com>
2017-03-29 21:09:50 -0700

    4 of them now

--- TouchIQ/Assets/Scripts/Views/ContactsList.cs
+++ TouchIQ/Assets/Scripts/Views/ContactsList.cs
@@ -26,7 +26,37 @@ public class ContactsList : MonoBehaviour
         Test.GetComponent<RectTransform>().localPosition = new Vector3(0, 0, 0);
         Test.GetComponent<RectTransform>().localScale = new Vector3(.35f, .35f, 1);
         Test.transform.Find("Circle").GetComponent<RectTransform>().localPosition = new Vector3(0, 500);
-        Test.transform.Find("Circle").GetComponent<RectTransform>().localRotation = Quaternion.Euler(0, 0, -30);
-        Test.GetComponent<RectTransform>().localRotation = Quaternion.Euler(0, 0, 30);
+        Test.transform.Find("Circle").GetComponent<RectTransform>().localRotation = Quaternion.Euler(0, 0, -20);
+        Test.GetComponent<RectTransform>().localRotation = Quaternion.Euler(0, 0, 20);
+
+        GameObject Test1 = Instantiate(Resources.Load<GameObject>("2.0/FrontPageButtons/Bubble")) as GameObject;
+
+        Test1.GetComponent<RectTransform>().SetParent(MiddleDial.transform);
+
+        Test1.GetComponent<RectTransform>().localPosition = new Vector3(0, 0, 0);
+        Test1.GetComponent<RectTransform>().localScale = new Vector3(.35f, .35f, 1);
+        Test1.transform.Find("Circle").GetComponent<RectTransform>().localPosition = new Vector3(0, 500);
+        Test1.transform.Find("Circle").GetComponent<RectTransform>().localRotation = Quaternion.Euler(0, 0, 20);
+        Test1.GetComponent<RectTransform>().localRotation = Quaternion.Euler(0, 0, -20);
+
+        GameObject Test2 = Instantiate(Resources.Load<GameObject>("2.0/FrontPageButtons/Bubble")) as GameObject;
+
+        Test2.GetComponent<RectTransform>().SetParent(MiddleDial.transform);
+
+        Test2.GetComponent<RectTransform>().localPosition = new Vector3(0, 0, 0);
+        Test2.GetComponent<RectTransform>().localScale = new Vector3(.35f, .35f, 1);
+        Test2.transform.Find("Circle").GetComponent<RectTransform>().localPosition = new Vector3(0, 500);
+        Test2.transform.Find("Circle").GetComponent<RectTransform>().localRotation = Quaternion.Euler(0, 0, -60);
+        Test2.GetComponent<RectTransform>().localRotation = Quaternion.Euler(0, 0, 60);
+
+        GameObject Test3 = Instantiate(Resources.Load<GameObject>("2.0/FrontPageButtons/Bubble")) as GameObject;
+
+        Test3.GetComponent<RectTransform>().SetParent(MiddleDial.transform);
+
+        Test3.GetComponent<RectTransform>().localPosition = new Vector3(0, 0, 0);
+        Test3.GetComponent<RectTransform>().localScale = new Vector3(.35f, .35f, 1);
+        Test3.transform.Find("Circle").GetComponent<RectTransform>().localPosition = new Vector3(0, 500);
+        Test3.transform.Find("Circle").GetComponent<RectTransform>().localRotation = Quaternion.Euler(0, 0, 60);
+        Test3.GetComponent<RectTransform>().localRotation = Quaternion.Euler(0, 0, -60);
     }
 }
