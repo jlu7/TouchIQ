a71b8ef24d414fd06fcae756aa8eeaf1ba75d912
Tonton Rue <tontonrue@playstudios.com>
2017-04-05 20:42:39 -0700

    popsicle now can go up and down

---
+++ TouchIQ/Assets/Scripts/Views/Popsicle.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Popsicle : MonoBehaviour
+{
+    Button ButtonComponent;
+    RectTransform View;
+    bool ViewIsVisible = true;
+
+    IEnumerator IEShowScrollView;
+
+    void Start()
+    {
+        ButtonComponent = this.transform.Find("Tab").GetComponent<Button>();
+        ButtonComponent.onClick.AddListener(ShowScrollView);
+        View = this.GetComponent<RectTransform>();
+    }
+
+    // Show is at y = 0
+    // Hidden is at y = -406
+    protected void ShowScrollView()
+    {
+        if (IEShowScrollView != null)
+        {
+            StopCoroutine(IEShowScrollView);
+        }
+        IEShowScrollView = null;
+        IEShowScrollView = coShowScrollView();
+
+        StartCoroutine(IEShowScrollView);
+    }
+    protected IEnumerator coShowScrollView()
+    {
+        int y = 0;
+
+        if (!ViewIsVisible)
+        {
+            y = -406;
+        }
+
+        ViewIsVisible = !ViewIsVisible;
+        Debug.Log(ViewIsVisible);
+
+        float speed = 2000;
+        Vector2 target = new Vector2(View.anchoredPosition.x, y);
+
+        while (View.anchoredPosition.y != y)
+        {
+            Debug.Log("LDSJF:LKDSJF");
+            float step = speed * Time.deltaTime;
+            View.anchoredPosition = Vector2.MoveTowards(View.anchoredPosition, target, step);
+            yield return null;
+        }
+
+    }
+}
