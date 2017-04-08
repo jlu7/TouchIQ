660fb791a3947620197bffafee1a8cdd6326dab3
Tonton Rue <tontonrue@playstudios.com>
2017-04-08 12:59:39 -0700

    UserPanel drag now works

---
+++ TouchIQ/Assets/Scripts/Utility/DragHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System;
+
+public class DragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public GameObject itemBeingDragged;
+    Vector3 startPosition;
+    Vector3 Difference = new Vector3();
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        itemBeingDragged = gameObject;
+        startPosition = transform.GetComponent<RectTransform>().anchoredPosition;
+        Difference = transform.position - Input.mousePosition;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition + Difference;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        itemBeingDragged = null;
+        transform.GetComponent<RectTransform>().anchoredPosition = startPosition;
+    }
+}
