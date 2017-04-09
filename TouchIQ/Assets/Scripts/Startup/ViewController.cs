2a0e066035b279d831eb68ea2590a498f37db910
Tonton Rue <tontonrue@playstudios.com>
2017-04-08 17:28:12 -0700

    new views are now dead center

--- TouchIQ/Assets/Scripts/Startup/ViewController.cs
+++ TouchIQ/Assets/Scripts/Startup/ViewController.cs
@@ -49,8 +49,10 @@ public class ViewController : MonoBehaviour
         }
         
         NewView.transform.parent = AnchorRef;
-        //NewView.transform.localScale = new Vector3(1, 1, 1);
-        //NewView.transform.localPosition = new Vector3(0, 0, 0);
+        NewView.GetComponent<RectTransform>().anchorMax = new Vector2(.5f, .5f);
+        NewView.GetComponent<RectTransform>().anchorMin = new Vector2(.5f, .5f);
+        NewView.GetComponent<RectTransform>().pivot = new Vector2(.5f, .5f);
+        NewView.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, 0);
 
         Views.Push(NewView.transform);
         CurrentView = NewView;
