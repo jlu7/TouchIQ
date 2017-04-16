74fd67f1a17af1e4cecf8477fff7ffd072b045ad
Tonton Rue <tontonrue@playstudios.com>
2017-04-15 17:50:25 -0700

    Incoming call screen now has spinning dots

--- TouchIQ/Assets/Scripts/Views/IncomingCall.cs
+++ TouchIQ/Assets/Scripts/Views/IncomingCall.cs
@@ -12,7 +12,7 @@ public class IncomingCall : MonoBehaviour
     {
         SoundManager.GetInstance().PlaySingle("SoundFX/Ringing_Phone", true);
 
-        acceptButton = this.transform.Find("Panel/BottomBar2/AcceptCall").GetComponent<Button>();
+        acceptButton = this.transform.Find("Panel/BottomBar/AcceptCall").GetComponent<Button>();
         acceptButton.onClick.AddListener(() =>
         {
             SoundManager.GetInstance().StopAllLoopingSoundEffects();
@@ -20,12 +20,27 @@ public class IncomingCall : MonoBehaviour
             ViewController.GetInstance().CreateView("Prefabs/SeniorCall/SeniorCall");
         });
 
-        declineButton = this.transform.Find("Panel/BottomBar2/EndCall").GetComponent<Button>();
+        declineButton = this.transform.Find("Panel/BottomBar/EndCall").GetComponent<Button>();
         declineButton.onClick.AddListener(() =>
         {
             SoundManager.GetInstance().StopAllLoopingSoundEffects();
             SoundManager.GetInstance().PlaySingle("SoundFX/pop_drip");
             ViewController.GetInstance().CreateView("Prefabs/ContactsScreen/ContactsScreen");
         });
+
+        StartCoroutine(RotateDotRings());
+    }
+
+    protected IEnumerator RotateDotRings()
+    {
+
+        RectTransform DotRing = transform.Find("MiddleDial/DotRing").GetComponent<RectTransform>();
+
+        //float speed = 500;
+        while (true)
+        {
+            DotRing.Rotate(Vector3.forward * Time.deltaTime * 4);
+            yield return null;
+        }
     }
 }
