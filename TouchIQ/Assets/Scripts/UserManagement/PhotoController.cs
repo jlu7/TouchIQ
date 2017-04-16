f61c2d00670570cdd5fcc0d9f55df21f22a1fbc5
greg <greg@playstudios.com>
2017-04-15 17:37:58 -0700

    added icons and a photolibrary object

---
+++ TouchIQ/Assets/Scripts/UserManagement/PhotoController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhotoController : MonoBehaviour {
+
+    private static PhotoController photoController;
+
+    public static PhotoController GetInstance()
+    {
+        if (photoController == null)
+        {
+            photoController = new GameObject("PhotoController").AddComponent<PhotoController>();
+        }
+        return photoController;
+    }
+
+    public void Initialize()
+    {
+        SpeechController.GetInstance().OnContextSpeechDetected += OnPhotoContextWord;
+    }
+
+    private void OnPhotoContextWord(List<string> contextWords)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ TouchIQ/Assets/Scripts/UserManagement/PhotoLibrary.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhotoLibrary : MonoBehaviour {
+
+    public PhotoSet[] PhotoSets;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+}
+
+[System.Serializable]
+public class PhotoSet
+{
+    public string Name;
+    public Texture2D[] Photos;
+
+}
